91053279d2314045477fcdb08c28a1eb7d53d75a
ashaantony8 <asha.antony@techversantinfotech.com>
2022-11-02 11:33:40 +0530

    copied

--- src/CapitalLetter.js
+++ src/CapitalLetter.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import ClipboardJS from "clipboard";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
@@ -13,10 +14,13 @@ export default function TextForm(props) {
     let newText2 = text.toLowerCase();
     setText(newText2);
   };
-  
-  const [text, setText] = useState("Enter Text Here");
+
+  const [text, setText] = useState("");
   return (
     <>
+      <div className="container my-3">
+        <h1> Text Conversion</h1>
+      </div>
       <div>
         <h1>{props.heading}</h1>
         <div className="form-group">
@@ -28,16 +32,16 @@ export default function TextForm(props) {
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+        <button className="btn btn-secondary mx-1" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+        <button className="btn btn-primary mx-1 " onClick={handleLowClick}>
           Convert to Lowercase
         </button>
-        
-      </div>
-      <div className="container my-3">
-        <h1>Your Text Summery</h1>
+        <button onClick={() => navigator.clipboard.writeText(`${text}`)}>
+          Copy
+        </button>
+        <h3>Text summary</h3>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
